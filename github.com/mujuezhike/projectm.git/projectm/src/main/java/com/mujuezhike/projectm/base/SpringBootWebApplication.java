b8dc6d869a1aff6e0d56fba2cd78b62bd3c7c7aa
mujuezhike <37540006@qq.com>
2017-09-29 17:56:05 +0800

    增加 druid连接池  监控

--- github.com/mujuezhike/projectm.git/projectm/src/main/java/com/mujuezhike/projectm/base/SpringBootWebApplication.java
+++ github.com/mujuezhike/projectm.git/projectm/src/main/java/com/mujuezhike/projectm/base/SpringBootWebApplication.java
@@ -2,7 +2,11 @@ package com.mujuezhike.projectm.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+//import core.data.DruidStatFilter;
+//import core.data.DruidStatViewServlet;
 
 /**
  * 容器中的spring启动类
@@ -10,8 +14,12 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * @date   20170927
  */
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootWebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
+    	
         SpringApplication.run(SpringBootWebApplication.class, args);
+        
     }
+
 }
